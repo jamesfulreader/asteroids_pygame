9d31414f668506f40663db4f43d1f602b9cdc2b2
jfulreader <jamesfulreader@gmail.com>
2024-09-24 20:02:13 -0600

    add movement to player

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
-  pygame.init()
-  clock = pygame.time.Clock()
-  dt = 0
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
-    screen.fill((0,0,0))
-    pygame.display.flip()
-    dt = (clock.tick(60) / 1000)
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+        player.update(dt)
+
 
 if __name__ == "__main__":
     main()
